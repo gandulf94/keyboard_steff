73dc24dd7cacc3f72f597ea2e67539c5068414ef
micha4el-git <michael.dahsler@t-online.de>
2020-02-06 23:42:02 +0100

    moved to express

--- website/node.js
+++ website/node.js
@@ -1,39 +1,36 @@
-var http = require('http');
-var url = require('url');
+// imports
+var express = require('express');
+var app = express();
 var fs = require('fs');
 
-  
-var server = http.createServer(function (req, res) {
-  const settings = JSON.parse(fs.readFileSync("settings.json"));
+// constants
+const appRootDir = "/node";
+const settings = JSON.parse(fs.readFileSync("settings.json"));
 
-  const queryObject = url.parse(req.url,true).query;
-  const queryFile = url.parse(req.url,true).pathname;
-  const sender = queryObject.sender;
+app.get('/node/record', function (req, res) {
+  //res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
+  const sender = req.query.sender;
   const user = settings.user;
-  console.log("queryFile: "+queryFile);
-  console.log("sender: "+sender);
-  console.log("user: "+user);
-  
-  if(queryFile != "/record") {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var msg = "call /record?sender=sender to record a new message";
-    res.end(msg);  
-    console.log(msg);
+
+  // check for valid sender
+  if(!user.includes(sender)) {
+    res.write('<h1>who are you:</h1>\n'); 
+    user.forEach(function(user_) {
+      res.write("<a href=record?sender="+user_+">"+user_+"</a>\n");
+      console.log(req.hostname+req.path);
+      console.log();
+    })
+    res.end();  
     return;
   }
-  if(!user.includes(sender)) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var msg = "unvalid user for recording";
-    res.end(msg);  
-    console.log(msg);
-    return; 
-  }
+
+  // list of possible receivers
+  receiverList = user.filter(user_ => user_ !== sender);
+
   
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end("valid user for recording"); 
-  console.log(user);
-  
+res.end("asdf"); 
+
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8080);
+app.listen(8086);
